0407972abb8270e2fc5edba0cde51dcf09625284
emily <emily@bacheconsulting.com>
2025-03-19 10:44:16 +0100

    tests in same order as production code

--- csharp/DiagramPrinterTests/DiagramPrinterTests.cs
+++ csharp/DiagramPrinterTests/DiagramPrinterTests.cs
@@ -4,14 +4,6 @@ namespace DiagramPrinterTests;
 
 public class DiagramPrinterTests
 {
-
-    [Test]
-    public void PrintingEmptyDocumentFails()
-    {
-        var printer = new DiagramPrinter.DiagramPrinter();
-        var result = printer.PrintDiagram(null);
-        Assert.IsFalse(result);
-    }    
     
     [Test]
     public void TranslatingEmptyDocumentFails()
@@ -21,4 +13,13 @@ public class DiagramPrinterTests
         var result = printer.PrintSummary(null, "swedish", ref output);
         Assert.IsFalse(result);
     }
+    
+    [Test]
+    public void PrintingEmptyDocumentFails()
+    {
+        var printer = new DiagramPrinter.DiagramPrinter();
+        var result = printer.PrintDiagram(null);
+        Assert.IsFalse(result);
+    }    
+
 }
